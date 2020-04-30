e0cf41bb1fbf6661c94d53863c8a1256664c73b5
ogunduz1 <64080312+ogunduz1@users.noreply.github.com>
2020-04-30 12:36:47 +0300

    5x8 binary matris
    -rastgele 5x8 lik binary matris oluşturur.

---
+++ matris1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+//rastgele 5x8 lik binary matris oluşturur.
+int main() {
+	int r;
+	for(int i=0;i<5;i++) {//i satır değeri için
+		for(int j=0;j<8;j++) {//j sütun değeri için
+			r=rand()%2;
+			printf("%d",r);//asıl değer işleme bu satırda yapılır-satır satır işleme
+		}
+	printf("\n");
+	}
+	return 0;
+}
